07279165ddb67c52611b1442a0ec888a75028bc8
Renato Mignogna <renmignogna@gmail.com>
2023-02-28 23:17:12 +0000

    homepage layout, sidebar added and some styling

--- app/head.tsx
+++ app/head.tsx
@@ -1,10 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>Chat GPT Clone 🚀</title>
-      <meta content="width=device-width,
-      initial-scale=1" name="viewport" />
+      <title>ChatGPT Clone</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
     </>
-  )
+  );
 }
